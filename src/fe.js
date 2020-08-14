ab885dd6e9dd93979914a0c41652a55097108dd4
Adam <carnagey.adam@gmail.com>
2020-08-14 16:42:10 -0500

    security questions being able to add 1 to 3.

--- src/fe.js
+++ src/fe.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import Login from './components/pages/login';
+import Register from './components/pages/register';
 // import './index.scss';
 
 render(
-    <Login />,
+    <Register />,
     document.getElementById('root')
 );
