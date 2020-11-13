e095441f7a49fcc557709bfa79256c54df767319
Adam Carnagey <carnagey.adam@gmail.com>
2020-11-13 16:39:49 -0600

    social support only if relevant

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -37,11 +37,32 @@ class UserController {
   getLoginInfoByUsernameOrEmail = async (request, response) => {
     try {
       const usernameOrEmail = request.body.usernameOrEmail;
-
       let res = await common.dbClient.getLoginInfoByUsernameOrEmail(
         usernameOrEmail
       );
-
+      // Don't show social support option if the owner has none enabled.
+      res.loginMethods = res.loginMethods.filter(lm => lm !== 'SocialSupportType');
+      try {
+        const user = await common.dbClient.findUserByUsernameOrEmail(
+          usernameOrEmail
+        );
+        const url = `${process.env.BACKEND_URI}/social-support/enabled`;
+        const init = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            secret: process.env.AUTH_SECRET,
+            username: user.username,
+          }),
+        };
+        let res2 = await fetch(url, init);
+        const data = await res2.json();
+        if(data.isEnabled) {
+          res.loginMethods.push('SocialSupportType');
+        }
+      } catch (err) {
+        console.error(err.stack);
+      }
       return response.json(res);
     } catch (err) {
       console.error(err.stack);
