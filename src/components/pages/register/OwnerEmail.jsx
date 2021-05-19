094003a283040c70bfd8d74414004feae8c38c8d
Adam Carnagey <adam.carnagey@transmute.industries>
2021-05-18 21:09:28 -0500

    styling and diff error handling behavior

--- src/components/pages/register/OwnerEmail.jsx
+++ src/components/pages/register/OwnerEmail.jsx
@@ -1,9 +1,10 @@
-import React, { Component, Fragment } from 'react';
-import { handleIOSBrowser } from '../../../util/browser-util';
-import GoBackSvg from '../../svg/GoBackSvg';
-import { animateIn, getSectionClassName } from '../../../util/animation-util';
+import React, { Component, Fragment } from "react";
+import { handleIOSBrowser } from "../../../util/browser-util";
+import GoBackSvg from "../../svg/GoBackSvg";
+import { animateIn, getSectionClassName } from "../../../util/animation-util";
+import UrlUtil from "../../../util/url-util";
 if (process.env.BROWSER) {
-  import('./OwnerEmail.scss');
+  import("./OwnerEmail.scss");
 }
 
 export default class OwnerEmail extends Component {
@@ -15,16 +16,25 @@ export default class OwnerEmail extends Component {
   constructor(props) {
     super(props);
     const email =
-      props.emailItem && props.emailItem.email ? props.emailItem.email : '';
+      props.emailItem && props.emailItem.email ? props.emailItem.email : "";
     const username =
       props.emailItem && props.emailItem.username
         ? props.emailItem.username
-        : '';
+        : "";
     const useEmail = props.emailItem ? !!props.emailItem.useEmail : true;
+    let error;
+    if (UrlUtil.getQueryVariable("error") === "user-exists") {
+      if (UrlUtil.getQueryVariable("type") === "email") {
+        error = "emailExists";
+      } else {
+        error = "usernameExists";
+      }
+    }
     this.state = {
       email,
       username,
       useEmail,
+      error,
     };
   }
 
@@ -34,7 +44,12 @@ export default class OwnerEmail extends Component {
   }
 
   render() {
-    const { email, username, useEmail } = { ...this.state };
+    const { email, username, useEmail, error } = { ...this.state };
+    const { appSettings } = { ...this.props };
+    const titleSetting = appSettings.find(
+      (a) => a.settingName === "title"
+    );
+    const title = titleSetting ? titleSetting.settingValue : undefined;
     return (
       <div
         ref="section"
@@ -54,7 +69,7 @@ export default class OwnerEmail extends Component {
                     e-mail account?
                   </Fragment>
                 ) : (
-                  'What username would you like to use to login?'
+                  "What username would you like to use to login?"
                 )}
               </div>
               {useEmail && (
@@ -67,15 +82,40 @@ export default class OwnerEmail extends Component {
             <div className="email-section">
               <div className="card-body">
                 <div className="card-body-section">
-                  <div>{useEmail ? 'E-mail' : 'Username'}</div>
+                  {error && (
+                    <>
+                      <div className="error">
+                        The{" "}
+                        {error === "emailExists" ? "email address" : "username"}{" "}
+                        is already being used
+                        {title ? ` on ${title}` : ""}, please click on "Go Back"
+                        or
+                      </div>
+                      <a
+                        href={
+                          location.origin +
+                          "/login" +
+                          location.search.replace(
+                            "success=false",
+                            "success=true"
+                          )
+                        }
+                      >
+                        login here
+                      </a>
+                    </>
+                  )}
+                  <div>{useEmail ? "E-mail" : "Username"}</div>
                   {useEmail ? (
                     <input
+                      className={error ? "error" : ""}
                       type="email"
                       value={email}
                       onChange={(e) => this.setState({ email: e.target.value })}
                     />
                   ) : (
                     <input
+                      className={error ? "error" : ""}
                       type="text"
                       value={username}
                       onChange={(e) =>
@@ -88,7 +128,7 @@ export default class OwnerEmail extends Component {
             </div>
             <div className="submit-section">
               <input
-                style={{ width: '210px', marginTop: '27px' }}
+                style={{ width: "210px", marginTop: "27px" }}
                 type="button"
                 value="Next"
                 disabled={
@@ -96,19 +136,19 @@ export default class OwnerEmail extends Component {
                   (!useEmail && username.length <= 0)
                 }
                 onClick={() =>
-                  this.props.handleGoForward('owner', 3, {
+                  this.props.handleGoForward("owner", 3, {
                     emailItem: { email, username, useEmail },
                   })
                 }
               />
               <div onClick={() => this.setState({ useEmail: !useEmail })}>
-                {useEmail ? "I don't have an e-mail" : 'I rather use e-mail'}
+                {useEmail ? "I don't have an e-mail" : "I'd rather use e-mail"}
               </div>
             </div>
           </div>
           <GoBackSvg
             color="#2362c7"
-            goBack={() => this.props.handleGoBack('owner', 2)}
+            goBack={() => this.props.handleGoBack("owner", 2)}
           />
         </div>
       </div>
