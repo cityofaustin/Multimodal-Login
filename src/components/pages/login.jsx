e348b041a399cb2dbab50377226219c24b85950c
Adam Carnagey <carnagey.adam@gmail.com>
2021-07-24 13:40:00 -0500

    16 can create owner account with only text login method. fixing uri instead of url

--- src/components/pages/login.jsx
+++ src/components/pages/login.jsx
@@ -319,10 +319,10 @@ class Login extends React.Component {
                 value={UrlUtil.getQueryVariable("response_type")}
               />
               <input
-                id="redirect_url"
-                name="redirect_url"
+                id="redirect_uri"
+                name="redirect_uri"
                 type="hidden"
-                value={UrlUtil.getQueryVariable("redirect_url")}
+                value={UrlUtil.getQueryVariable("redirect_uri")}
               />
               <input id="scope" name="scope" type="hidden" value="" />
               <input id="state" name="state" type="hidden" value="" />
